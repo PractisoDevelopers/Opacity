75aef4e0058fceca5d01950f581e64f60838694e
zhufucdev <zhufuzhufu1@gmail.com>
2025-06-25 07:34:17 +0800

    :bug: missing fill-in step

--- prisma/migrations/20250624144721_add_time_info_to_archive/migration.sql
+++ prisma/migrations/20250624144721_add_time_info_to_archive/migration.sql
@@ -1,12 +1,12 @@
-/*
-  Warnings:
-
-  - Added the required column `updateTime` to the `Archive` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
-ALTER TABLE "Archive" ADD COLUMN     "updateTime" TIMESTAMP(3) NOT NULL,
+ALTER TABLE "Archive" ADD COLUMN     "updateTime" TIMESTAMP(3),
 ADD COLUMN     "uploadTime" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- Fill-in
+UPDATE "Archive" SET "updateTime" = CURRENT_TIMESTAMP WHERE "updateTime" IS NULL;
+
+-- Add constraint
+ALTER TABLE "Archive" ALTER COLUMN "updateTime" SET NOT NULL;
 
 -- CreateIndex
 CREATE INDEX "Archive_updateTime_idx" ON "Archive" USING BRIN ("updateTime");
