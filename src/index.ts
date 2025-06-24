cc5688bd617fa3d2823491d384e745d8bcf570f3
zhufucdev <zhufuzhufu1@gmail.com>
2025-06-24 16:33:36 +0800

    :sparkles: whoami

--- src/index.ts
+++ src/index.ts
@@ -114,4 +114,21 @@ app.put('/upload', async (c) => {
 	return c.json(returnJson);
 });
 
+app.get('/whoami', async (c) => {
+	const {cid} = c.get('jwtPayload');
+	if (!cid) {
+		throw new HTTPException(401);
+	}
+	const prisma = usePrismaClient(c.env.DATABASE_URL);
+	const client = await prisma.client.findUnique({
+		where: { id: cid },
+		include: { owner: { select: { name: true } } },
+	});
+	if (!client) {
+		throw new HTTPException(403);
+	}
+
+	return c.json({ clientName: client.name, name: client.owner.name });
+});
+
 export default app satisfies ExportedHandler<Env>;
