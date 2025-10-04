a05fa85df35af4e4555a04d56131cdd5679a830d
zhufucdev <zhufuzhufu1@gmail.com>
2025-10-04 16:14:51 +0800

    :sparkles: report magic values in bonjour

--- src/endpoints/bonjour.ts
+++ src/endpoints/bonjour.ts
@@ -1,8 +1,11 @@
 import { Hono } from 'hono';
 import * as compat from '../compat';
+import { maxNameLength, pageSize } from '../magic';
+
+const reportedMagicValues = `page_size:${pageSize} max_name_length:${maxNameLength}`;
 
 export function useBonjour(app: Hono<OpacityEnv>) {
 	app.get('/bonjour', async (c) => {
-		return c.text(`opacity version:${compat.version} build_date:${compat.buildDate}`);
+		return c.text(`opacity version:${compat.version} build_date:${compat.buildDate} ${reportedMagicValues}`);
 	});
 }
